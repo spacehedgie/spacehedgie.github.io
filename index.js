325e1a3e87cf5c5283d0fef8ac1db19b6eae9cbb
spacehedgie <j.spdgie@gmail.com>
2025-02-23 15:04:05 -0600

    Additional stuff added

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 console.log("Hi there")
+const siteSquares = document.getElementsByClassName("squares");
 
 function openHedgie() {
     window.open('/spacehedgieMain/hedgieHome.html')
+}
+
+// place addEventListener to all "squares" elements (use for getElementsByClassName)
+for (var i = 0 ; i < siteSquares.length; i++) {
+    if (siteSquares == event.target) {
+        siteSquares[i].addEventListener("mouseover", event => {
+            event.target.style.background = "linear-gradient(rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))"
+        });
+    }
+}
+
+for (var i = 0 ; i < siteSquares.length; i++) {
+    siteSquares[i].addEventListener("mouseout", event => {
+        event.target.style.background = "rgba(255, 255, 255, 0.3)"
+    });
 }
