c4b682321784d24db2fee44bc8fa0a5affa167c4
spacehedgie <j.spdgie@gmail.com>
2025-03-02 19:43:22 -0600

    spacehedgie & spacehedgie Music site designs. More visual changes and mobile support updates.

--- index.js
+++ index.js
@@ -7,18 +7,3 @@ function openHedgie() {
 function openMusic() {
     window.open('/spacehedgieMusic/musicHome.html')
 }
-
-// place addEventListener to all "squares" elements (use for getElementsByClassName)
-for (var i = 0 ; i < siteSquares.length; i++) {
-    if (siteSquares == event.target) {
-        siteSquares[i].addEventListener("mouseover", event => {
-            event.target.style.background = "linear-gradient(rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))"
-        });
-    }
-}
-
-for (var i = 0 ; i < siteSquares.length; i++) {
-    siteSquares[i].addEventListener("mouseout", event => {
-        event.target.style.background = "rgba(255, 255, 255, 0.3)"
-    });
-}
