f9ba4901dfe49f20f13b5646eb6a851e03a8357a
spacehedgie <j.spdgie@gmail.com>
2025-08-29 00:11:17 -0500

    Page redirection fix + intro transition animations

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 console.log("Hi there")
 const siteSquares = document.getElementsByClassName("squares");
 let docCover = document.getElementById('coverAnim');
-let animPlaying = false;
 
 function coverScreen(callback) {
-    animPlaying = true;
     docCover.classList.add('transitionAnim');
     docCover.addEventListener('animationend', () => {
-        docCover.classList.remove('transitionAnim');
-        animPlaying = false;
         callback();
     });
 }
 
 function openHedgie() {
-    window.open('/Main/Home.html');
+    window.open('/Main/Home.html', '_self');
 }
 function openMusic() {
-    window.open('/Music/Home.html');
+    window.open('/Music/Home.html', '_self');
 }
